19f12e75a0c1f86c0bfb6a6a5982a8414f68fbb7
ankitpradhan123 <ankit.03996@gmail.com>
2025-04-20 20:08:59 +0530

    added .gitignore and static files config

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from app import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('disclaimer/', views.disclaimer, name='disclaimer'),
     path('privacy-policy/', views.privacy_policy, name='privacy-policy')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- youtify/settings.py
+++ youtify/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-357=ys&#40paco^q6ej$^@mj3*+@zp!&(mm)i#j#iyo1*m22+='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -111,6 +111,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
