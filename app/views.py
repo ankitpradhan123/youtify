e64d699bc7daac0dd546a8dda7581d7445431ab5
ankitpradhan123 <ankit.03996@gmail.com>
2025-04-26 20:58:35 +0530

    added optional linkedin content generation with toggle button feature

--- app/views.py
+++ app/views.py
@@ -13,13 +13,15 @@ def index(request):
         }
         body = json.loads(request.body)
         youtube_url = body.get("youtube_url")
+        generate_linkedin_post = body.get("generate_linkedin_post")
         content_generator_obj = ContentGenerator(youtube_url)
         captions = content_generator_obj.generate_docs_from_transcript()
         if not captions:
             response["error"] = "No captions found"
             return JsonResponse(response)
         response["blog_post"] = content_generator_obj.generate_blog_post(captions)
-        response["linkedin_post"] = content_generator_obj.generate_linkedin_post(captions)
+        if generate_linkedin_post:
+            response["linkedin_post"] = content_generator_obj.generate_linkedin_post(captions)
         return JsonResponse(response)
     return render(request, 'index.html')
 
